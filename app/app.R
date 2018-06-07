c4516e25480600aed355e132ba4a2bae1326cc83
Joshua Rosenberg <jrosen@msu.edu>
2018-06-07 12:23:57 -0400

    update

--- app/app.R
+++ app/app.R
@@ -24,54 +24,79 @@ library(shiny)
 #     distinct(name, state, distance, category, surface, n_reviews, mean_raw_review, pred_review, URL)
 # write_rds(dd, "app/processed_data.rds")
 dd <- readr::read_rds("processed_data.rds")
+dd$n_reviews <- as.integer(stringr::str_extract(dd$n_reviews, "\\(?[0-9,.]+\\)?"))
+
 # dd %>%
 #     gather(type_of_review, val, -name, -state) %>%
 #     ggplot(aes(x = val, fill = type_of_review)) +
 #     geom_density(alpha = .3) +
 #     theme_bw()
 
+options(DT.options = list(pageLength = 10))
+
 # Define UI for application that draws a histogram
-ui <- fluidPage(theme = shinythemes::shinytheme("cerulean"),
-                titlePanel("Find the best rail-trails in each state"),
-                p("Choose a state to view the top rail-trails based on reviews from the Trail Link website"),
-                p("If you like using this, please consider visiting or even making a donation to the Rails to Trails Conservancy at via https://www.traillink.com/"),
+ui <- fluidPage(theme = shinythemes::shinytheme("yeti"),
+                titlePanel("Find the top rail-trails (using reviews from TrailLink)"),
+                p("Choose a state (or click the 'All states' checkbox) to view the top rated rail-trails based on predictions from a multi-level model."),
 
                 # Sidebar with a slider input for number of bins
                 sidebarLayout(
                     sidebarPanel(
                         selectInput("state", "State", dd$state %>% unique(), "MI"),
-                        p("Rail-trails data and reviews are available from the railtrails R package: https://jrosen48.github.io/railtrails/"),
-                        p("The source code for this app is available here: https://jrosen48.github.io/railtrails/")
-                    ),
+                        checkboxInput("overall", "All states"),
+                        p("Rail-trails data and reviews are available from the railtrails R package:", tags$a("https://jrosen48.github.io/railtrails/")),
+                          p("The source code for this app is available here:", tags$a("https://github.com/jrosen48/railtrails")),
+                          p("If you like using this, please consider visiting or even making a donation to the Rails to Trails Conservancy via", tags$a("https://www.traillink.com/"))
+                        ),
 
-                    # Show a plot of the generated distribution
-                    mainPanel(
-                        dataTableOutput("df")
+                        # Show a plot of the generated distribution
+                        mainPanel(
+                            dataTableOutput("df")
+                        )
                     )
-                ),
-                p("Note: The review is a prediction from a mixed effects, or multi-level, model, that considers how many high reviews there are (and how variable they are) because using the raw reviews would lead to trails with a few very positive reviews being ranked as the best.")
 
-)
+                )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+                # Define server logic required to draw a histogram
+                server <- function(input, output) {
 
-    output$df <- renderDataTable({
-        dd %>%
-            filter(state == input$state) %>%
-            filter(!is.na(pred_review)) %>%
-            arrange(desc(pred_review)) %>%
-            select(State = state,
-                   `Trail name` = name,
-                   `Distance (mi.)` = distance,
-                   `Surface` = surface,
-                   `Number of reviews` = n_reviews,
-                   `Review` = pred_review,
-                   `Map` = URL) %>%
-            mutate(Review = round(Review, 3))
-    })
-}
+                    output$df <- renderDataTable({
 
-# Run the application
-shinyApp(ui = ui, server = server)
+                        if (input$overall) {
+
+                            dd %>%
+                                filter(!is.na(pred_review)) %>%
+                                arrange(desc(pred_review)) %>%
+                                select(State = state,
+                                       `Trail name` = name,
+                                       `Length (mi.)` = distance,
+                                       `Surface` = surface,
+                                       `Number of reviews` = n_reviews,
+                                       `Review` = pred_review,
+                                       `Trailhead map` = URL) %>%
+                                mutate(`Trailhead map` = paste0("<a href='", `Trailhead map` ,"' target='_blank'>", `Trailhead map`,"</a>"),
+                                       Review = round(Review, 3))
+
+                        } else {
+
+                            dd %>%
+                                filter(state == input$state) %>%
+                                filter(!is.na(pred_review)) %>%
+                                arrange(desc(pred_review)) %>%
+                                select(State = state,
+                                       `Trail name` = name,
+                                       `Length (mi.)` = distance,
+                                       `Surface` = surface,
+                                       `Number of reviews` = n_reviews,
+                                       `Review` = pred_review,
+                                       `Trailhead map` = URL) %>%
+                                mutate(`Trailhead map` = paste0("<a href='", `Trailhead map` ,"' target='_blank'>", `Trailhead map`,"</a>"),
+                                       Review = round(Review, 3))
+
+                        }
+                    }, escape = FALSE)
+                }
+
+                # Run the application
+                shinyApp(ui = ui, server = server)
 
